1215f8be12666ad26364f601fd2421d09d104a16
Julian Meyer <julianmeyer2000@gmail.com>
2019-09-26 16:40:22 -0700

    Switch to unified config system

--- explorer/cmd/synapseexplorer.go
+++ explorer/cmd/synapseexplorer.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"strings"
+
+	"github.com/phoreproject/synapse/p2p"
 	"github.com/phoreproject/synapse/utils"
 
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -11,6 +16,13 @@ func main() {
 	utils.CheckNTP()
 
 	config := explorer.LoadConfig()
+	changed, newLimit, err := utils.ManageFdLimit()
+	if err != nil {
+		panic(err)
+	}
+	if changed {
+		logrus.Infof("changed ulimit to: %d", newLimit)
+	}
 
 	ex, err := explorer.NewExplorer(config)
 	if err != nil {
